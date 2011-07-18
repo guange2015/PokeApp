cc52330382231255af6704ef24f0ed52678fe4c6
hhuai <8863824@gmail.com>
2011-07-18 23:01:47 +0800

    .

--- message.rb
+++ message.rb
@@ -28,22 +28,115 @@ require 'cstruct'
 # +  **STRING** : 
 #        不定长,根据具体的描述
 #
+# ### 消息头
+#
+#     字段名        中文名     类型  备注
+#     Total_Length  包长度     INT 
+#     Command_ID    消息类型   SHORT 
+#     Sequence_ID   消息流水   INT   交互所产生的序号,连接不断就不变
+#     STATUS        状态       BYTE  状态
+# 
 # 字段定义
 # --------
 # ### 跟注状态(Rep_Status)
-# + *跟注*:  0x1
-# + *放弃*:  0x2
-# + *全押*:  0x4
-# + *加注*:  0x8
+# 
+#     跟注:  0x1
+#     放弃:  0x2
+#     全押:  0x4
+#     加注:  0x8
 # 
 # ### 消息类型(Command_ID):
 #
-#     登陆          WIN_LOGIN             0x1
-#     回应登陆      WIN_LOGIN_REP         0x801
-#     下发牌请求    WIN_PUSH_POKE         0x2
-#     下发牌回复    WIN_PUSH_POKE_REP     0x802
-#     上传跟注状态  WIN_PUSH_STATUS       0x3
-#     回应跟注状态  WIN_PUSH_STATUS_REP   0x803
+#     消息              字段名                    值 
+#     登陆              WIN_LOGIN                 0x1
+#     回应登陆          WIN_LOGIN_REP             0x801
+#     下发牌请求        WIN_PUSH_POKE             0x2
+#     下发牌回复        WIN_PUSH_POKE_REP         0x802
+#     上传跟注状态      WIN_PUSH_STATUS           0x3
+#     回应跟注状态      WIN_PUSH_STATUS_REP       0x803
+#     广播跟注状态      WIN_BROADCAST_STATUS      0x4
+#     回应广播跟注状态  WIN_BROADCAST_STATUS_REP  0x804
+#
+# ### 牌定义:
+#
+#     1 黑桃8   8   红桃8   15  梅花8   22  方块8
+#     2 黑桃9   9   红桃9   16  梅花9   23  方块9
+#     3 黑桃10  10  红桃10  17  梅花10  24  方块10
+#     4 黑桃J   11  红桃J   18  梅花J   25  方块J
+#     5 黑桃Q   12  红桃Q   19  梅花Q   26  方块Q
+#     6 黑桃K   13  红桃K   20  梅花K   27  方块K
+#     7 黑桃A   14  红桃A   21  梅花A   28  方块A
+#
+# ### 状态 STATUS
+# 
+#     成功    :  0
+#     认证错误:  1
+#     其他错误:  9
+#     桌子已满:  2
+#     参数错误:  3
+#
+#
+# 消息交互
+# --------
+# 
+# ### 客户端登陆-请求
+# 
+#     字段名 类型    长度  备注
+#     IMEI   STRING  15  
+# 
+# ### 客户端登陆-回应
+# 
+#     字段名    类型   长度      备注
+#     Desk_ID   SHORT  桌子号
+#     SMALL     INT    最小金额
+#     BIG       INT    最大金额
+#     Client_ID BYTE             客户端序号, 2人的话，就是1和2
+# 
+# ### 下发牌请求 (服务器->客户端)
+# 
+#     字段名              类型  长度   备注
+#     Client_Count        BYTE         客户端个数,一般为2
+#     YOU_CLIENT_ID       BYTE         你的CLIENT_ID
+#     SHOW_CLINET_ID      BYTE         本次该谁出牌
+#     DESKTOP_MONEY       INT          桌子上押的金额
+#     Client1_ID          BYTE         客户端1的ID
+#     Client1_HIDE_POKER  BYTE         参见牌定义，此字段只有第一次下发才有值
+#     Client1_POKER_CODE  BYTE         参见牌定义
+#     Client1_POST_MONEY  INT          已押注
+#     Client1_LAST_MONEY  INT          本次剩余金额
+#     此处是重复Client1,有几次重复应该根据Client_Count来判断
+#
+# ### 下发牌回应(客户端->服务器)
+# 
+#     字段名              类型  长度  备注
+#     Client_ID           BYTE    
+# 
+# ### 跟注状态上传(客户端->服务器)
+# 
+#     字段名      类型  长度  备注
+#     Client_ID   BYTE    
+#     Rep_Status  BYTE        跟注状态
+#     Money       INT         跟多少金额，主要是为了获取加注数
+#
+## ### 跟注状态回应(服务器->客户端)
+# 
+#     字段名      类型  长度  备注
+#     Client_ID   BYTE    
+#
+#
+## ### 广播跟注状态(服务器->客户端)
+# 
+#     字段名           类型  长度   备注
+#     Client_ID        BYTE    
+#     Rep_Status       BYTE         跟注状态
+#     Money            INT          跟多少金额，主要是为了获取加注数
+#     CAN_Rep_Status   BYTE         当前能够使用的状态
+#
+## ### 回应广播跟注状态(客户端->服务器)
+# 
+#     字段名           类型  长度  备注
+#     Client_ID        BYTE        回复你的ID
+#
 module Message
 class MessageHeader < CStruct
   options :endian => :big
--- test/message_spec.rb
+++ test/message_spec.rb
@@ -4,8 +4,8 @@ require File.expand_path('../test_helper.rb',__FILE__)
 require 'message'
 
 describe Message do
-    context "Mesage Header" do
-        it "message can service" do
+    context "消息头结构" do
+        it "消息头结构" do
             msg_header = Message::MessageHeader.new{ |m|
                 m.total_length = 0x1
                 m.command_id = 0x123
@@ -17,8 +17,32 @@ describe Message do
         end
     end
     
-    context "Message Login" do
-        it "message login can service" do
+    context "客户端登陆消息结构" do
+        it "客户端登陆-请求" do
+            pending
+        end
+        it "客户端登陆-回应" do
+            pending
         end
     end
+
+    context "下发牌消息结构" do
+        it "下发牌请求 (服务器->客户端)" do
+            pending
+        end
+    end
+
+    context "跟注状态消息结构" do
+        it "跟注状态上传(客户端->服务器)" do
+            pending
+        end
+    end
+
+
+    context "广播跟注消息结构" do
+        it "广播跟注状态(服务器->客户端)" do
+            pending
+        end
+    end
+
 end
